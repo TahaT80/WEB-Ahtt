451de395bcbbb2cda7a240016384001bca405bc1
Amir Hossein Tadas <1380.tadas@gmail.com>
2024-02-27 14:16:29 +0330

    total

--- src/App.js
+++ src/App.js
@@ -1,24 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
-
+import {useEffect, useState} from "react";
+import useMediaQuery from "./hooks/useMediaQuery";
+import Navbar from "./scense/navbar";
+import GropDot from "./scense/GropDot";
+import Landing from "./scense/Landing";
+import LineGradient from "./componnent/LineGradient";
+import MySkills from "./scense/MySkills";
+import Projects from "./scense/Projects";
+import Testimonials from "./scense/Testimonials";
+import Contact from "./scense/Contact";
+import Footer from "./scense/Footer";
 function App() {
+    const [selectPage , setselectPage] = useState('home')
+    const [isTopOfPage , setisTopOfPage] = useState(true)
+    const isDesktop = useMediaQuery("(min-width: 1060px)");
+
+    useEffect(() => {
+        const handelscroll = ()=>{
+            if (window.scrollY === 0) {
+                setisTopOfPage(true)
+            }
+            if (window.scrollY !== 0) {
+                setisTopOfPage(false)
+            }
+        };
+        window.addEventListener("scroll",handelscroll);
+        return ()=>window.removeEventListener("scroll",handelscroll);
+    }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className={'app bg-deep-blue'}>
+          <Navbar
+              isDesktop={isDesktop}
+              selectedPage={selectPage}
+              setSelectedPage={setselectPage}
+              isTopOfPage={isTopOfPage}
+          />
+
+          <div className={''}>
+              {isDesktop && (
+                  <GropDot
+                      selectedPage={selectPage}
+                      setSelectedPage={setselectPage}/>
+              )}
+          </div>
+          <Landing
+              selectedPage={selectPage}
+              setSelectedPage={setselectPage}/>
+          <LineGradient/>
+          <div className={'w-5/6 mx-auto md:h-full'}>
+              <MySkills></MySkills>
+          </div>
+          <LineGradient/>
+          <div className={'w-5/6 mx-auto'}>
+              <Projects></Projects>
+          </div>
+          <LineGradient/>
+          <div className={'w-5/6 mx-auto'}>
+              <Testimonials></Testimonials>
+          </div>
+          <LineGradient/>
+          <div className={'w-5/6 mx-auto'}>
+              <Contact></Contact>
+          </div>
+          <LineGradient/>
+          <div>
+              <Footer></Footer>
+          </div>
+      </div>
   );
 }
 
--- src/componnent/SocialMediaIcons.jsx
+++ src/componnent/SocialMediaIcons.jsx
@@ -2,8 +2,38 @@ import React from 'react';
 
 const SocialMediaIcons = () => {
     return (
-        <div>
-            
+        <div className="flex justify-center md:justify-start my-10 gap-7">
+            <a
+                className="hover:opacity-50 transition duration-500"
+                href="https://www.linkedin.com"
+
+            >
+                <img alt="linkedin-link" src="../assets/linkedin.png"/>
+            </a>
+            <a
+                className="hover:opacity-50 transition duration-500"
+                href="https://www.twitter.com"
+                target="_blank"
+                rel="noreferrer"
+            >
+                <img alt="twitter-link" src="../assets/twitter.png"/>
+            </a>
+            <a
+                className="hover:opacity-50 transition duration-500"
+                href="https://www.facebook.com"
+                target="_blank"
+                rel="noreferrer"
+            >
+                <img alt="facebook-link" src="../assets/facebook.png"/>
+            </a>
+            <a
+                className="hover:opacity-50 transition duration-500"
+                href="https://www.instagram.com"
+                target="_blank"
+                rel="noreferrer"
+            >
+                <img alt="instagram-link" src="../assets/instagram.png"/>
+            </a>
         </div>
     );
 };
